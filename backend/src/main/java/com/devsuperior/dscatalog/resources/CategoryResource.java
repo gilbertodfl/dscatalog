76a439af37d6cdcb98607e628ca05f9dbbdd417f
Gilberto de Freitas Lins <gilbertodefreitaslins@hotmail.com>
2021-05-24 22:48:34 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,5 +1,4 @@
 package com.devsuperior.dscatalog.resources;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
@@ -18,13 +17,13 @@ public class CategoryResource {
 	@Autowired
 	private CategoryService service;
 	@GetMapping
-	public ResponseEntity <List<Category> >findAll(){
+	public ResponseEntity <List<CategoryDTO> >findAll(){
 		
 		/*List<Category> list = new ArrayList<>();
 		list.add(new Category ( 1L, "BOOKS"));
 		list.add(new Category ( 2L, "ELECTRONICS"));
 		*/
-		List<Category> list = service.findAll();
+		List<CategoryDTO> list = service.findAll();
 		return  ResponseEntity.ok().body(list);
 		
 	}
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -18,7 +21,16 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository; 
 	
-	public List<Category> findAll(){
-		return repository.findAll();
+	@Transactional( readOnly = true )
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		List<CategoryDTO> ListDto = list.stream().map( x  -> new CategoryDTO(x) ).collect(Collectors.toList());
+		/* o código acima do map faz exatamente o que faria nas linhas abaixo que estão em comentário.
+		List<CategorDTO> listDto = new ArrayList<>();
+		for( Category cat: list ) {
+			listDto.add( new CategoryDTO(cat));
+		}
+		*/
+		return ListDto;
 	}
 }
