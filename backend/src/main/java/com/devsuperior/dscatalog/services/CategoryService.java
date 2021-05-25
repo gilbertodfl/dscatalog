372b223ca8689b14caeee9f3e6150aecf0d5521b
Gilberto de Freitas Lins <gilbertodefreitaslins@hotmail.com>
2021-05-25 16:00:35 -0300

    Update category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 /* Esta notation já prepara para determinar que o scrping vai gerenciar 
  * Outras opções seriam: @Component  @Repository
@@ -40,7 +40,7 @@ public class CategoryService {
 	@Transactional( readOnly = true )
 	public CategoryDTO findById(Long id) {
 		Optional <Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow( ()->  new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow( ()->  new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
 	@Transactional
@@ -49,5 +49,18 @@ public class CategoryService {
 		entity.setName( dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO( entity);
+	}
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		// com o getOnde você ainda não vai no banco, poupando o acesso
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save( entity );
+			return new CategoryDTO( entity );
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id );
+		}
 	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
@@ -3,11 +3,11 @@ package com.devsuperior.dscatalog.services.exceptions;
 
 // exception : tem que ser obrigatoriamente tratada. 
 // RuntimeException : tratado em tempo de execução
-public class EntityNotFoundException extends RuntimeException {
+public class ResourceNotFoundException extends RuntimeException {
 
 	
 	private static final long serialVersionUID = 1L;
-	public EntityNotFoundException ( String msg )
+	public ResourceNotFoundException ( String msg )
 	{
 		super(msg);
 	}
